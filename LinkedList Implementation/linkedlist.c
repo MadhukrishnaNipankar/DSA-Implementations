77179ce2bea369e8c8793e38f95bc98909a18b0e
Madhukrishna Nipankar <nipankarmadhu@gmail.com>
2022-11-26 00:06:12 +0530

    linkedlist operations in c

--- LinkedList Implementation/linkedlist.c
+++ LinkedList Implementation/linkedlist.c
@@ -31,16 +31,46 @@ struct node *insertAtEnd(struct node *head)
     int eleAtEnd;
     printf("\nEnter Value of element at End: ");
     scanf("%d", &eleAtEnd);
-    new->data=eleAtEnd;
-    new->next=NULL;
+    new->data = eleAtEnd;
+    new->next = NULL;
 
     while (temp->next != NULL)
     {
         temp = temp->next;
     }
-    
-    temp->next = new;
 
+    temp->next = new;
+}
+
+struct node *deleteFromBeginning(struct node *head)
+{
+    head = head->next;
+    return head;
+}
+
+void insertAtMid(struct node *head)
+{
+    struct node *new, *temp, *preserver;
+    new = (struct node *)malloc(sizeof(struct node *));
+
+    int eleAtMid;
+    printf("\nEnter Value of element to insert at mid: ");
+    scanf("%d", &eleAtMid);
+    new->data = eleAtMid;
+    new->next = NULL;
+
+    int position;
+    printf("\nEnter the position to insert : ");
+    scanf("%d", &position);
+
+    for (int i = 1; i < position; i++)
+    {
+        temp = temp->next;
+    }
+
+    preserver = temp->next;
+    temp->next = new;
+    new->next = preserver;
 }
 
 void displayLinkedList(struct node *head)
@@ -48,7 +78,7 @@ void displayLinkedList(struct node *head)
     struct node *temp;
     temp = head;
     printf("\nList Elements are: \n");
-    
+
     while (temp != NULL)
     {
         printf("%d\n", temp->data);
@@ -77,6 +107,11 @@ int main()
 
     insertAtEnd(head);
 
+    insertAtMid(head);
+
     displayLinkedList(head);
+
+    // head = deleteFromBeginning(head);
+
     return 0;
 }
